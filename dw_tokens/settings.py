60da6e414c9b9ef83bc7305ace38783f690073ec
pAbilash <abilash.pakalapati@gmail.com>
2021-01-29 00:22:12 +0530

    staticfile

--- dw_tokens/settings.py
+++ dw_tokens/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 'data' is my media folder
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"globalstaticfiles"),
+]
+
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # 'data' is my media folder
